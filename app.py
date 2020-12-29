a36bb1423142d4dca224b81e02f3a9db388e81d1
Igor Košmerl <i.kosmerl@gmail.com>
2020-12-29 21:19:09 +0100

    Loaded and parsed symptoms from file

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import json
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = []
+    file = open("static/assets/files/ds_symptoms.txt", "r")
+    symptoms = file.readlines()
+    for s in symptoms:
+        data.append(s.replace("'","").replace("_", " ").replace(",\n", ""))
+    data = json.dumps(data)
+    return render_template('index.html', data=data)
