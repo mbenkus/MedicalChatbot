0047e538f4ded994a4d3822c716ce2b191142121
Igor Košmerl <i.kosmerl@gmail.com>
2020-12-29 23:36:20 +0100

    Implemented autocomplete, symptoms data needs forwarding to script

--- static/js/main.js
+++ static/js/main.js
@@ -17,3 +17,94 @@ $(document).ready(function () {
     }
   });
 });
+
+function getSymptomsData(data) {
+  //todo
+}
+
+function autocomplete(input, symptomsData) {
+  var currentFocus;
+
+  input.addEventListener("input", function (e) {
+    var a, b, i, val = this.value;
+
+    closeAllLists();
+
+    if (!val) {
+      return false;
+    }
+
+    currentFocus = -1;
+    a = document.createElement("div");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    this.parentNode.appendChild(a);
+
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        b = document.createElement("div");
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+        b.addEventListener("click", function (e) {
+          input.value = this.getElementsByTagName("input")[0].value;
+          closeAllLists();
+        });
+
+        a.appendChild(b);
+      }
+    }
+  });
+  
+  input.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+
+    // On arrow down, arrow up and enter
+    if (e.keyCode == 40) {
+      currentFocus++;
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      currentFocus--;
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      e.preventDefault();
+      if (currentFocus > -1) {
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+
+  // Defines item as active
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = x.length - 1;
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+
+  // Removes active state from all autocomplete items
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != input) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+}
+
+// todo fill symptomsdata
+autocomplete(document.getElementById("message-text"), symptomsData);
