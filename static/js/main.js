920929a42e5a7ecd367d700523f418211a88edca
Igor Košmerl <i.kosmerl@gmail.com>
2021-01-02 16:11:47 +0100

    Restructured js and formatted HTML

--- static/js/main.js
+++ static/js/main.js
@@ -1,66 +1,59 @@
 $(document).ready(function () {
-
   symptoms = JSON.parse(symptoms);
-  var dataList = document.getElementById("symptoms-list");
+  let input = $("#message-text");
+  let dataList = $("#symptoms-list");
+  let suggestedItem = $("#symptoms-list li");
 
   $(".symptoms-list-container").css("display", "none");
 
-  $("#send").click(function () {
-    $.fn.handleUserMessage();
+  input.on("input", function () {
+    let insertedValue = $(this).val();
+    $("#symptoms-list").empty();
+
+    if (insertedValue.length > 1) {
+      ssymptoms = $.fn.getSuggestedSymptoms(insertedValue);
+      for (let i = 0; i < ssymptoms.length; i++) {
+        var li = document.createElement("li");
+        li.textContent = ssymptoms[i];
+        dataList.append(li);
+      }
+    }
   });
 
-  $("#message-text").on("keypress", function (e) {
+  input.on("focus", function () {
+    $(".symptoms-list-container ").css("display", "block");
+  });
+
+  //todo - check why it does not work
+  suggestedItem.click(function () {
+    console.log("test");
+  });
+
+  input.on("keypress", function (e) {
     if (e.which == 13) {
       $.fn.handleUserMessage();
     }
   });
 
-  $.fn.getSuggestedSymptoms = function (val) {
-    let suggestedSymptoms = []
-    $.each(symptoms, function(i,v) {
-      if (v.includes(val)) {
-        suggestedSymptoms.push(v);
-      }
-    });
-    return suggestedSymptoms.slice(0,3);
-  }
-
-  $("#message-text").on("input", function () {
-    let insertedValue = $(this).val();
-    $("#symptoms-list").empty();
-    if (insertedValue.length > 2) {
-      ssymptoms = $.fn.getSuggestedSymptoms(insertedValue);
-      for (let i = 0; i < ssymptoms.length; i++) {
-        var li = document.createElement("li");
-        li.textContent = ssymptoms[i];
-        dataList.appendChild(li);
-      }
-    }
-  });
-
-  $("#message-text").on("focus", function () {
-    $(".symptoms-list-container ").css("display", "block");
-  });
-
-  $("#message-text").on("blur", function () {
-    $(".symptoms-list-container").css("display", "none");
+  $("#send").click(function () {
+    $.fn.handleUserMessage();
   });
 
   $.fn.handleUserMessage = function () {
     $.fn.appendUserMessage();
     $.fn.getPredictedSymptom();
-    $("#message-text").val("");
+    input.val("");
   }
 
   $.fn.appendUserMessage = function () {
-    var tekst = $("#message-text").val();
+    var tekst = input.val();
     $("#conversation").append(
       `<div class="row message-body"><div class="col-sm-12 message-main-sender"><div class="sender"><div class="message-text">${tekst}</div></div></div></div>`
     );
   }
 
   $.fn.getPredictedSymptom = function () {
-    var tekst = $("#message-text").val();
+    var tekst = input.val();
     $.ajax({
       url: "http://127.0.0.1:5000/symptom",
       data: JSON.stringify({ "sentence": tekst }),
@@ -77,5 +70,15 @@ $(document).ready(function () {
         console.log("Error");
       }
     });
+  }
+
+  $.fn.getSuggestedSymptoms = function (val) {
+    let suggestedSymptoms = []
+    $.each(symptoms, function(i,v) {
+      if (v.includes(val)) {
+        suggestedSymptoms.push(v);
+      }
+    });
+    return suggestedSymptoms.slice(0,3);
   }
 });
